6bd71a5c006192075f4bf3f8c0fa04db7d785c86
JerryWang <wzr@bu.edu>
2023-06-27 00:51:47 -0400

    start

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -3,32 +3,54 @@ package standalone_storage
 import (
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"path"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	engine *engine_util.Engines
+	conf   *config.Config
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	dirPath := conf.DBPath
+	kvPath := path.Join(dirPath, "kv")
+	raftPath := path.Join(dirPath, "raft")
+
+	// get new db
+	kvDB := engine_util.CreateDB(kvPath, false)
+	raftDB := engine_util.CreateDB(raftPath, true)
+
+	sas := &StandAloneStorage{
+		engine: engine_util.NewEngines(kvDB, raftDB, kvPath, raftPath),
+	}
+	return sas
 }
 
 func (s *StandAloneStorage) Start() error {
 	// Your Code Here (1).
+
 	return nil
 }
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
+	err := s.engine.Close()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
+	txn := s.engine.Kv.NewTransaction(false)
+
 	return nil, nil
 }
 
